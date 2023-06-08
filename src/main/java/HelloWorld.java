eca4a18c362953804fb57023bcbda74229112ea9
VladimirCherik <vladimir_cherik@i.ua>
2023-06-08 18:09:16 +0300

    Changed HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("I\nlove\nVictoriya");
+        System.out.println("it's so interesting");
     }
 
 
